7d4e93a847d0efadd3da4ded89efff23a207098b
Juan Franco Cavallieri <54825138+jfcava@users.noreply.github.com>
2025-11-19 09:41:35 -0300

    Ejercicios 1,2,3,4 OK

---
+++ 09 Recursividad/ejercicio2.py
@@ -0,0 +1,14 @@
+# Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición
+# indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario
+# especifique.
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print("=== CALCULO DE SERIE DE FIBONACCI ===")
+posicion_final = int(input("Indique la posicion final: "))
+
+for i in range(posicion_final + 1):
+    print(f"Valor en posicion {i}: {fibonacci(i)}")
---
+++ 09 Recursividad/ejercicio3.py
@@ -0,0 +1,17 @@
+# Crea una función recursiva que calcule la potencia de un número base elevado a un
+# exponente, utilizando la fórmula 𝑛
+# 𝑚 = 𝑛 ∗ 𝑛(𝑚−1)
+# . Prueba esta función en un algoritmo general.
+
+def potencia(n, m):
+    if m == 0:
+        return 1
+    else:
+        return n * potencia(n, m - 1)
+    
+numero = int(input("Ingrese un numero: "))
+potencia_seleccionada = int(input("Ingrese la potencia: "))
+
+print(potencia(numero, potencia_seleccionada))
+    
+
